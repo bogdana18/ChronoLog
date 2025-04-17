697d12c881ce7f28ce33c9f3c59dd53614d543b9
Bohdana Usenko <usenkobogdana19@mail.com>
2025-04-17 23:18:02 +0300

    fix: app settings

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,17 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
-  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
